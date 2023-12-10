97ea8b47f30ce36599bd7038a9e0d735a0847899
UtmostCreator <romazahrypa@gmail.com>
2023-12-10 14:57:48 +0000

    init commit

---
+++ js4Edindex.js
@@ -0,0 +1,131 @@
+const worstLocations = ['Lochend', 'Inch', 'West Pilton', 'Granton', 'Muirhouse', 'Clermiston/Parkgrove', 'Greendykes', 'Royston Mains', 'Southhouse/Burdiehous', 'Restalrig', 'Milton', 'Dumbryden', 'Calders', 'Hyvots', 'Hailesland', 'Murrayburn', 'Saughton Mains',
+  'Prestonfield', 'Craigmillar', 'Niddrie', 'Moredun', 'Gilmerton', 'Gracemount',
+  'Bingham, Magdalene and The Christians', 'Stenhouse', 'Saughton', 'Broomhouse', 'Wester Hails', 'Inch'
+];
+
+// base settings
+const valueClass = '.field-value-min';
+const propertyItemClass = '.table-row-min';
+
+// good, needed
+const moreChancesArr = ['Starter'];
+const roomTypeFilterArr = ["One", "Studio"];
+const propertyFilteredTypesArr = ["Starter", "Either Starter or Mover"];
+
+// not good
+const notTheBestLevelArr = ['Basement', 'Ground'];
+const notSuitableArr = ['Mover'];
+const removeWhenArr = ['Aged 60 and over', 'Sheltered'];
+const warningArr = ['Fourth', 'Multi storey flat']; // floor
+
+$(document).ready(function() {
+  console.log('ready');
+
+  function runCheck() {
+    $(propertyItemClass).each(function() {
+      const that = $(this);
+      let roomTypeOk = false;
+      let propTypeOk = false;
+      let worstPlace = false;
+      let notSuitable = false;
+      let removeThis = false;
+      let moreChances = false;
+      let notTheBestLevel = false;
+      let warning = false;
+      let roomStudio = false;
+      $(this).find(valueClass).each(function() {
+        let text = $(this).text().trim();
+        if (roomTypeFilterArr.includes(text)) {
+          roomTypeOk = true;
+          if (text === 'Studio') {
+            roomStudio = true;
+            $(this).css('color', 'yellow');
+          }
+        }
+        if (removeWhenArr.includes(text)) {
+          removeThis = true;
+          return;
+        }
+
+        if (text === 'House') {
+          $(this).text($(this).text() + '🏡');
+          $(this).css('font-size', '18px');
+        }
+
+        if (worstLocations.includes(text)) {
+          worstPlace = true;
+        }
+        console.log('worstPlace', worstPlace);
+
+        if (propertyFilteredTypesArr.includes(text)) {
+          propTypeOk = true;
+        }
+        if (notSuitableArr.includes(text)) {
+          notSuitable = true;
+        }
+        if (moreChancesArr.includes(text) && propTypeOk && roomTypeOk) {
+          moreChances = true;
+        }
+        if (warningArr.includes(text)) {
+          warning = true;
+          $(this).css('color', 'orange');
+        }
+        if (notTheBestLevelArr.includes(text)) {
+          notTheBestLevel = true;
+          $(this).css('color', 'red');
+        }
+      });
+      if (!roomTypeOk || removeThis) {
+        $(this).remove();
+      }
+      if (notTheBestLevel || warning) {
+        $(this).css({
+          "border-color": "yellow",
+          "border-width": "4px",
+          "border-style": "solid"
+        });
+      }
+      if (moreChances && !worstPlace) {
+        $(this).css('background-color', 'green');
+        $(this).css({
+          "border-color": "blue",
+          "border-width": "4px",
+          "border-style": "solid"
+        });
+        $(this).parent().prepend($(this));
+      } else if (roomTypeOk && propTypeOk) {
+        if (worstPlace) {
+          $(this).css('background-color', 'grey');
+          $(this).parent().append($(this))
+        } else if (warning) {
+          $(this).css('background-color', 'burlywood');
+        } else {
+          $(this).css('background-color', 'yellowgreen');
+        }
+      } else if (worstPlace) {
+        $(this).css('background-color', 'red');
+        $(this).css('opacity', '.5');
+      } else if (notSuitable) {
+        $(this).css('background-color', 'yellow');
+        $(this).css('opacity', '.5');
+      }
+    })
+    $('html, body').animate({
+      scrollTop: 0
+    }, 'slow');
+  }
+
+
+  setTimeout(function() {
+    runCheck();
+  }, 1000);
+
+  $('#body-primary-region').on('click', 'td.pagination div.pagination', function() {
+    console.log('a clicked');
+    setTimeout(runCheck, 1000)
+  });
+
+  $('#body-primary-region').on('click', '.field-value-min', function() {
+    console.log('click');
+  });
+});
